fae22cf2cb1a5908df784dd6878ab42fc1944f57
Maciek <maciejciepal@gmail.com>
2023-01-02 15:40:25 +0100

    Add serving stored dish image

--- controllers/DishController.js
+++ controllers/DishController.js
@@ -1,4 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import Dish from "../models/Dish.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function getAllDishes(request, response) {
     const dishes = await Dish.find({});
@@ -12,6 +18,8 @@ export async function getAllDishes(request, response) {
 
 export async function createDish(request, response) {
     const newDish = new Dish(request.body);
+    newDish.photoUrls.push(request.file.filename)
+
     try {
         await newDish.save();
         response.status(200).send(newDish);
@@ -55,5 +63,23 @@ export async function deleteDish(request, response) {
         }
     } catch (err) {
         response.status(500).send(err);
+    }
+}
+
+export async function getDishPhotos(request, response) {
+    const dish = await Dish.findById(request.params.id);
+    console.log(__dirname)
+    try {
+        if (!dish) {
+            response.status(404).send("No item found!");
+        }
+        if (request.params.imageid >= dish.photoUrls.length) {
+            response.status(404).send("No image found!");
+        }
+        response.sendFile('image-1672572328872-908889981.jpeg', { 
+            root: path.join(__dirname, '../uploads') 
+        });
+    } catch (error) {
+        response.status(500).send(error);
     }
 }
--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+import upload from "./config/storage.js";
 import { getAllDishes, 
     createDish, 
     readDish, 
     updateDish, 
-    deleteDish } from "./controllers/DishController.js";
+    deleteDish,
+    getDishPhotos } from "./controllers/DishController.js";
 
 import "./config/db.js";
 
-import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
-
-app.use(cors())
-
 const port = process.env.PORT || 3301;
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app
-    .route("/dishes")
+app.route("/dishes")
     .get(getAllDishes)
-    .post(createDish);
+    .post(upload.single("image"), createDish);
 
-app
-    .route("/dishes/:id")
+app.route("/dishes/:id")
     .get(readDish)
     .put(updateDish)
     .delete(deleteDish);
+
+app.route("/dishes/:id/:imageid")
+    .get(getDishPhotos);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
