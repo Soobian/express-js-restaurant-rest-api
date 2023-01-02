3e4bfff91a1501ec618bad12ae8a791edc7ec348
Maciek <maciejciepal@gmail.com>
2023-01-02 15:38:55 +0100

    Add multer storage

---
+++ config/storage.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.jpeg')
+    }
+})
+
+const upload = multer({ 
+    storage: storage,
+    limits : { fileSize : 10 * 1024 * 1024 }
+})
+
+export default upload;
